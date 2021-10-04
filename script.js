779eafaf5a3efe40377ec89b0025027c421da395
Nicolae Pop <nicupop729@gmail.com>
2021-10-04 18:23:53 -0300

    remove spaces and style the js file

--- script.js
+++ script.js
@@ -10,21 +10,19 @@ const createMyElement = (param) => {
   return document.createElement(param);
 };
 
-
-
 // Create a div:
-const menuDiv = createMyElement('div');
+const menuDiv = createMyElement("div");
 menuDiv.className = "toggle-nav";
 // In the div create a ul
-const menuUl = createMyElement('ul');
+const menuUl = createMyElement("ul");
 // In the ul we will create 3lis
-const menuPortfolio = createMyElement('li');
+const menuPortfolio = createMyElement("li");
 menuPortfolio.textContent = "Portfolio";
-const menuAbout = createMyElement('li');
+const menuAbout = createMyElement("li");
 menuAbout.textContent = "About";
-const menuContact = createMyElement('li');
+const menuContact = createMyElement("li");
 menuContact.textContent = "Contact";
-const closeIcon = createMyElement('span');
+const closeIcon = createMyElement("span");
 closeIcon.textContent = "X";
 closeIcon.id = "closeIcon-id";
 
@@ -33,38 +31,34 @@ menuUl.appendChild(menuPortfolio);
 menuUl.appendChild(menuAbout);
 menuUl.appendChild(menuContact);
 
-
 //Append the ul in the Div:
 menuDiv.appendChild(closeIcon);
 menuDiv.appendChild(menuUl);
 
 const humbergerButton = getMyElement(".menu-icon");
 
-menuPortfolio.addEventListener("click", ()=>{
+menuPortfolio.addEventListener("click", () => {
   menuDiv.style.display = "none";
   document.location = "#portfolio";
 });
 
-menuAbout.addEventListener("click", ()=>{
+menuAbout.addEventListener("click", () => {
   menuDiv.style.display = "none";
   document.location = "#about";
-})
+});
 
-menuContact.addEventListener("click", ()=>{
+menuContact.addEventListener("click", () => {
   menuDiv.style.display = "none";
   document.location = "#contact";
-})
+});
 
 //Add evenlistener to the closeIcon:
-closeIcon.addEventListener("click", ()=>{
+closeIcon.addEventListener("click", () => {
   menuDiv.style.display = "none";
-})
-
-
+});
 
 humbergerButton.addEventListener("click", () => {
   document.body.appendChild(menuDiv);
   menuDiv.style.backgroundColor = "#fff";
   menuDiv.style.display = "block";
-  // console.log(menuToggleLi);
 });
