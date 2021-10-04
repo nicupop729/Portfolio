428932d0d149ef74a898b49c658def4ca7b2a747
Nicolae Pop <nicupop729@gmail.com>
2021-10-04 18:49:57 -0300

    Fix errors showed by linter check

--- script.js
+++ script.js
@@ -1,64 +1,50 @@
-const getMyElement = (param) => {
-  return document.querySelector(param);
-};
+const getMyElement = param => document.querySelector(param)
 
-const getAllMyElement = (param) => {
-  return document.querySelectorAll(param);
-};
+const createMyElement = param => document.createElement(param)
 
-const createMyElement = (param) => {
-  return document.createElement(param);
-};
+const menuDiv = createMyElement('div')
+menuDiv.className = 'toggle-nav'
+const menuUl = createMyElement('ul')
+const menuPortfolio = createMyElement('li')
+menuPortfolio.textContent = 'Portfolio'
+const menuAbout = createMyElement('li')
+menuAbout.textContent = 'About'
+const menuContact = createMyElement('li')
+menuContact.textContent = 'Contact'
+const closeIcon = createMyElement('span')
+closeIcon.textContent = 'X'
+closeIcon.id = 'closeIcon-id'
 
-// Create a div:
-const menuDiv = createMyElement("div");
-menuDiv.className = "toggle-nav";
-// In the div create a ul
-const menuUl = createMyElement("ul");
-// In the ul we will create 3lis
-const menuPortfolio = createMyElement("li");
-menuPortfolio.textContent = "Portfolio";
-const menuAbout = createMyElement("li");
-menuAbout.textContent = "About";
-const menuContact = createMyElement("li");
-menuContact.textContent = "Contact";
-const closeIcon = createMyElement("span");
-closeIcon.textContent = "X";
-closeIcon.id = "closeIcon-id";
+menuUl.appendChild(menuPortfolio)
+menuUl.appendChild(menuAbout)
+menuUl.appendChild(menuContact)
 
-//Append the lis to the ul:
-menuUl.appendChild(menuPortfolio);
-menuUl.appendChild(menuAbout);
-menuUl.appendChild(menuContact);
+menuDiv.appendChild(closeIcon)
+menuDiv.appendChild(menuUl)
 
-//Append the ul in the Div:
-menuDiv.appendChild(closeIcon);
-menuDiv.appendChild(menuUl);
+const humbergerButton = getMyElement('.menu-icon')
 
-const humbergerButton = getMyElement(".menu-icon");
+menuPortfolio.addEventListener('click', () => {
+  menuDiv.style.display = 'none'
+  document.location = '#portfolio'
+})
 
-menuPortfolio.addEventListener("click", () => {
-  menuDiv.style.display = "none";
-  document.location = "#portfolio";
-});
+menuAbout.addEventListener('click', () => {
+  menuDiv.style.display = 'none'
+  document.location = '#about'
+})
 
-menuAbout.addEventListener("click", () => {
-  menuDiv.style.display = "none";
-  document.location = "#about";
-});
+menuContact.addEventListener('click', () => {
+  menuDiv.style.display = 'none'
+  document.location = '#contact'
+})
 
-menuContact.addEventListener("click", () => {
-  menuDiv.style.display = "none";
-  document.location = "#contact";
-});
+closeIcon.addEventListener('click', () => {
+  menuDiv.style.display = 'none'
+})
 
-//Add evenlistener to the closeIcon:
-closeIcon.addEventListener("click", () => {
-  menuDiv.style.display = "none";
-});
-
-humbergerButton.addEventListener("click", () => {
-  document.body.appendChild(menuDiv);
-  menuDiv.style.backgroundColor = "#fff";
-  menuDiv.style.display = "block";
-});
+humbergerButton.addEventListener('click', () => {
+  document.body.appendChild(menuDiv)
+  menuDiv.style.backgroundColor = '#fff'
+  menuDiv.style.display = 'block'
+})
